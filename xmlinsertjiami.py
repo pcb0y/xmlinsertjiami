20c4a27993b48b0eb9e8867bd2dbf6f0539bade0
pcb0y <pcb0y@163.com>
2023-04-27 11:30:35 +0800

    增加过滤18MM

--- xmlinsertjiami.py
+++ xmlinsertjiami.py
@@ -2,20 +2,22 @@ import xml.etree.ElementTree as ET
 import tkinter as tk
 from tkinter import filedialog
 import json
+import time
 
 # Create GUI window to select XML file
-root = tk.Tk()
+window = tk.Tk()
 # 设置窗口标题
-root.title("北方XML基材插入加密")
+window.title("北方XML基材插入加密")
 
 # 设置窗口大小
-root.geometry("400x100+500+150")
+window.geometry("400x120+500+150")
 color_dict = []
 with open("config.ini", 'r', encoding='utf-8') as f:
     color_str = f.read()
-    lb_color = tk.Label(root, text='', )
+    lb_color = tk.Label(window, text='', )
     lb_color.config(text=color_str)
     color_dict = json.loads(color_str)
+
 
 
 def update_xml(filename):
@@ -23,19 +25,26 @@ def update_xml(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     # Find node with attribute '花色'
+    num = 0
     for color in color_dict:
         bar_color = root.findall(f'.//Panel[@Material="{color}"]')
+
         for Panel in bar_color:
 
-            if Panel.attrib.get('BasicMaterial') == '实芯板':
+            if Panel.attrib.get('BasicMaterial') == '实芯板' and Panel.attrib.get('Thickness') == "18":
                 # 修改基材
                 Panel.set('BasicMaterial', '实芯板-加密')
+                num += 1
+
     tree.write(filename)
-    lb.config(text='修改成功!')
+    lb.config(text=f'修改成功!')
+
+    lb1 = tk.Label(window, text=f'{num}', font=("黑体", 30), foreground='#E33E30').pack()
 
 
 # 设置回调函数
 def openfile():
+
     # 从本地选择一个文件，并返回文件的目录
     filename = filedialog.askopenfilename()
     if filename != '':
@@ -46,9 +55,9 @@ def openfile():
 
 
 # 使用按钮控件调用函数
-b = tk.Button(root, text="打开北方XML文件", command=openfile).pack()
-lb = tk.Label(root, text='', )
+b = tk.Button(window, text="打开北方XML文件", command=openfile).pack()
+lb = tk.Label(window, text='', )
 lb.pack()
 lb_color.pack()
 
-root.mainloop()
+window.mainloop()
